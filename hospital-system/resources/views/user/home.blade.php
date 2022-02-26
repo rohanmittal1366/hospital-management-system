93f6e94f665555fe11e2139f58b3849a832987c3
rohanmittal_crest <rohan.mittal@crestdatasys.com>
2022-02-26 22:54:49 +0530

    Added home for user and admin, made changes in database and add controller for user and admin

---
+++ hospital-system/resources/views/user/home.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    <h1>This is user home</h1>
+</x-app-layout>
--- hospital-system/routes/web.php
+++ hospital-system/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [HomeController::class,'redirect']);
+
+
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
